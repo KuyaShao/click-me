a2b3710182df206c515ce79a91ae9af83ad39d79
KuyaShao <38858966+KuyaShao@users.noreply.github.com>
2023-05-30 00:55:40 +0800

    Adding Loading State

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -5,18 +5,21 @@ import axiosClient from "./axios";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     fetchCount();
   }, []);
   async function fetchCount() {
     try {
+      setLoading(true);
       const response = await axiosClient.get("/click-counts");
       if (response.status === 200) {
         setCount(response.data);
       }
+      setLoading(false);
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   }
   async function handleClick() {
@@ -29,11 +32,16 @@ function App() {
   }
   return (
     <div className="container">
-      <h1 className="title">Click Me App</h1>
-      <button className="button" onClick={handleClick}>
-        Click Me
-      </button>
-      <p className="count">{count}</p>
+      {loading && <h1 className="title">Loading...</h1>}
+      {!loading && (
+        <>
+          <h1 className="title">Click Me App</h1>
+          <button className="button" onClick={handleClick}>
+            Click Me
+          </button>
+          <p className="count">{count}</p>
+        </>
+      )}
     </div>
   );
 }
